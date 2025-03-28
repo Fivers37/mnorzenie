0e763eba1d763ab25ee3936593fa9d1da0f919e9
Fivers37 <wiktordroszcz2009@gmail.com>
2025-03-28 12:27:33 +0100

    a no poczontki

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 
 import './App.css'
+let a,b;
+let 
+w1=a*b
+w2=a*b
+w3=a*b
+w4=a*b
+w5=a*b
+w6=a*b
+w7=a*b
+w8=a*b
+w9=a*b
+w10=a*b;
 
 function App() {
-   return (
+  
+  
+  
+  return (
     <>
      hello, World
+     <p>hej</p>
     </>
   )
 }
